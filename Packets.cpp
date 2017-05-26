dc97108e56e4aa4d15babb785478f45a3872789a
elan <elan@localhost>
2017-05-26 20:14:23 +0200

    20% mehr netzwerk

--- Packets.cpp
+++ Packets.cpp
@@ -9,32 +9,15 @@ int readInt(Stream& st) {
 	return r;
 }
 
-bool ServerJoinAckPacket::readFromStream(Stream& st) {
-	this->playerId = st.read();
+
+void writeInt(Stream& st, uint32 in) {
+	for (int i=0; i<4; ++i) {
+		uint8 o = in & 0x000000FF;
+		st.write(o);
+		in >>= 8;
+	}
 }
 
-bool ServerJoinAckPacket::writeToStream(Stream& st) {
-	st.write(playerId);
-}
-
-
-bool ServerGameStartPacket::readFromStream(Stream& st) {
-	gameround = readInt(st);
-	text = st.readString();
-	led1 = readInt(st);
-	led2 = readInt(st);
-	led3 = readInt(st);
-	led4 = readInt(st);
-	timeoutseconds = readInt(st);
-}
-
-bool ServerClientActionLogPacket::readFromStream(Stream& st) {
-	stickdir = st.read();
-	deviceorientation = st.read();
-	playerid = st.read();
-}
-
-bool ClientActionPacket::readFromStream(Stream& st) {
-	stickdir = st.read();
-	deviceorientation = st.read();
+void writeString(Stream& st, String& s) {
+	st.write(s.c_str());
 }
--- Packets.h
+++ Packets.h
@@ -5,9 +5,19 @@
 
 #include <Stream.h>
 
+int readInt(Stream& st);
+void writeInt(Stream& st, uint32 in);
+void writeString(Stream& st, String& s);
 
+template <int TYPE>
 struct GamePacket
 {
+	virtual bool writeToStream(Stream& st) {
+		st.write(this->type);
+		this->writeToStreamInternal(st);
+	}
+	virtual bool writeToStreamInternal(Stream & st) = 0;
+	enum { type = TYPE};
 };
 
 
@@ -39,18 +49,22 @@ enum packet_types_t
 	PACKET_SGAMESUC
 };
 
-struct ServerJoinAckPacket : GamePacket
+struct ServerJoinAckPacket : GamePacket<PACKET_SJOIN>
 {	
 	ServerJoinAckPacket() {
 
 	}
 	
-	bool readFromStream(Stream& st);
-	bool writeToStream(Stream& st);
+	bool readFromStream(Stream& st) {
+		this->playerId = st.read();
+	}
+	bool writeToStreamInternal(Stream& st) {
+		st.write(playerId);
+	}
 	uint8 playerId;
 };
 
-struct ServerGameStartPacket : GamePacket
+struct ServerGameStartPacket : GamePacket<PACKET_SSTART>
 {	
 	ServerGameStartPacket() {
 
@@ -59,8 +73,24 @@ struct ServerGameStartPacket : GamePacket
 	gameround(gameround), text(text), led1(led1), led2(led2), led3(led3), led4(led4), timeoutseconds(timeoutseconds) {
 
 	}
-	bool readFromStream(Stream& st);
-	bool writeToStream(Stream& st);
+	bool readFromStream(Stream& st) {
+		gameround = readInt(st);
+		text = st.readString();
+		led1 = readInt(st);
+		led2 = readInt(st);
+		led3 = readInt(st);
+		led4 = readInt(st);
+		timeoutseconds = readInt(st);
+	}
+	bool writeToStreamInternal(Stream& st) {
+		writeInt(st, gameround);
+		writeString(st, text);
+		writeInt(st, led1);
+		writeInt(st, led2);
+		writeInt(st, led3);
+		writeInt(st, led4);
+		writeInt(st, timeoutseconds);
+	}
 	
 	int gameround;
 	String text;
@@ -71,19 +101,27 @@ struct ServerGameStartPacket : GamePacket
 	uint32 timeoutseconds;
 };
 
-struct ServerClientActionLogPacket : GamePacket
+struct ServerClientActionLogPacket : GamePacket<PACKET_SLOGACT>
 {	
 	ServerClientActionLogPacket() {
 
 	}
-	bool readFromStream(Stream& st);
-	bool writeToStream(Stream& st);
+	bool readFromStream(Stream& st) {
+		stickdir = st.read();
+		deviceorientation = st.read();
+		playerid = st.read();
+	}
+	bool writeToStreamInternal(Stream& st) {
+		st.write(stickdir);
+		st.write(deviceorientation);
+		st.write(playerid);
+	}
 	int stickdir;
 	int deviceorientation;
 	uint8 playerid;
 };
 
-struct ClientActionPacket : GamePacket {
+struct ClientActionPacket : GamePacket<PACKET_CACTION> {
 	ClientActionPacket(uint8 stickdir, uint8 deviceorientation) :
 		stickdir(stickdir), deviceorientation(deviceorientation) {
 
@@ -92,8 +130,14 @@ struct ClientActionPacket : GamePacket {
 	ClientActionPacket(): stickdir(0), deviceorientation(0) {
 
 	}
-	bool readFromStream(Stream& st);
-	bool writeToStream(Stream& st);
+	bool readFromStream(Stream& st) {
+		stickdir = st.read();
+		deviceorientation = st.read();
+	}
+	bool writeToStreamInternal(Stream& st) {
+		st.write(stickdir);
+		st.write(deviceorientation);
+	}
 	uint8 stickdir;
 	uint8 deviceorientation;
 };
